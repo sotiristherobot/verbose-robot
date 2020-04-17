25b99caaf8cb2064495ab5302dfaf8cad23dd168
Sotiris Ioannou <sotiris@jsmate.net>
2020-04-18 00:54:29 +0200

    Added <Point/>

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { map } from 'lodash';
+import Point from './components/Point';
+
+// data
+import pointData from './db/data.json';
 
 function App() {
+  const { data } = pointData;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {
+        map(data, (point: any) => {
+          const { x, y, gaze_id: key } = point;
+          return <Point x={x} y={y} key={key} />
+        })
+      }
     </div>
   );
 }
---
+++ src/components/Point.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+type Point = {
+    x: Number,
+    y: Number
+}
+
+const Point = ({ x, y }: Point) => {
+    const styles = {
+        backgroundColor: "red" as 'red',
+        borderRadius: "50%",
+        height: "50px",
+        width: "50px",
+        position: "absolute" as 'absolute',
+        left: x + 'px',
+        top: y + 'px'
+    };
+
+    return (
+        <div>
+            <div style={styles}></div>
+        </div>
+    )
+}
+export default Point;
